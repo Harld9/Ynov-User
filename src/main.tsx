a86be3e1bd96ade5e25f81ecaae15733a04472a0
Harold <harold.francois@ynov.com>
2026-09-15 17:09:56 +0200

    fix: suppression d'une route non utilisée

--- src/main.tsx
+++ src/main.tsx
@@ -28,10 +28,6 @@ const router = createBrowserRouter([
       element: <App />,
     },
     {
-      path: '/userlist',
-      element: <UserList />,
-    },
-    {
       path: '/user/:username',
       element: <User />,
     },
